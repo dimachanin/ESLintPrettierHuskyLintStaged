2571b2b92c482dc13eacf839ef1e5711faf6abd9
Dmitrii Khanin <Dmitrii_Khanin@epam.com>
2021-05-11 11:19:10 +0300

    Some default file with 'smelling code'

---
+++ src/index.js
@@ -0,0 +1,21 @@
+function returnA   (a)  {
+ return a
+}
+
+class dog  {
+
+    sayHaw ( b )  { 
+        console.log(name + ' say Haw!');
+}
+
+constructor(name)  {
+ this.name = name;
+}
+}
+
+let i = undefined;
+var i;
+
+const i = 
+() =>  
+3
